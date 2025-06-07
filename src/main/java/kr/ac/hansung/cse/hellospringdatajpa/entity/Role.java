b31b5bca4c44235328f0b22cb5c8e3ab472416a7
juhokim <line1559@naver.com>
2025-06-07 16:28:03 +0900

    Role lombok 추가 및 생성자 추가

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/Role.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/Role.java
@@ -1,11 +1,17 @@
 package kr.ac.hansung.cse.hellospringdatajpa.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "roles")
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +21,8 @@ public class Role {
 
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
+
+    public Role(String roleUser) {
+        this.rolename = roleUser;
+    }
 }
