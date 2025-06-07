d08801d8e6f9db5b8562ef6bcfdcc070841aeaeb
juhokim <line1559@naver.com>
2025-06-07 16:26:55 +0900

    User 유효성 검사 추가 및 Get,Set 추가

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/User.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/User.java
@@ -1,15 +1,23 @@
 package kr.ac.hansung.cse.hellospringdatajpa.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
     private String password;
 
