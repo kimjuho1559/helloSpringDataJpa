dfd73d5bb2ee98b2df46889c8b59ee73274a3fce
juhokim <line1559@naver.com>
2025-06-07 16:30:35 +0900

    회원관리 서비스 구현

---
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package kr.ac.hansung.cse.hellospringdatajpa.service;
+
+import kr.ac.hansung.cse.hellospringdatajpa.entity.User;
+import kr.ac.hansung.cse.hellospringdatajpa.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found"));
+        List<SimpleGrantedAuthority> authorities = user.getRoles()
+                .stream().map(r -> new SimpleGrantedAuthority(r.getRolename()))
+                .toList();
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(), authorities);
+    }
+}
